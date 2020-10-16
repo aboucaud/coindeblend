4fae32605ac2b74b0e8443cfd4a0d7e7295b8054
Alexandre Boucaud <3065310+aboucaud@users.noreply.github.com>
2020-10-16 14:58:15 +0200

    Fix security warning on tf version (#2)
    * Update environment.yml
    
    * Update requirements.txt
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license="BSD",
     install_requires = [
         "numpy",
-        "tensorflow<2.0",
+        "tensorflow==1.14",
         "keras",
         "h5py",
         "scikit-learn",
